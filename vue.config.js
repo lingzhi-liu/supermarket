fe4d9b90dbb8c52e6f412f9ae6d582c9f267420d
平天定海 <897010704@qq.com>
2024-05-13 12:21:31 +0800

    跟着coderwhy老师学习vue第151课：vue.config.js配置路径别名以及.editorconfig格式规范文件

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'api': '@/api',
+        'common': '@/common',
+        'components': '@/components',
+        'pages': '@/pages',
+      }
+    }
+  }
 })
